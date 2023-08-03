df99062d53996760c0107e53134a0a1a34064a0f
Mahyar Mirrashed <mah.mirr@gmail.com>
2023-08-03 15:00:54 -0500

    feat: change to use var() instead of env()
    The running project must set the values of `--safe-area-inset-{}` using the
    `capacitor-plugin-safe-area` plugin.

--- index.js
+++ index.js
@@ -1,68 +1,68 @@
 const plugin = require('tailwindcss/plugin')
 
-const safeArea = plugin(({addUtilities, matchUtilities, theme}) => {
+const safeArea = plugin(({ addUtilities, matchUtilities, theme }) => {
 	const baseUtilities = {
 		'.m-safe': {
-			marginTop: 'env(safe-area-inset-top)',
-			marginRight: 'env(safe-area-inset-right)',
-			marginBottom: 'env(safe-area-inset-bottom)',
-			marginLeft: 'env(safe-area-inset-left)',
+			marginTop: 'var(--safe-area-inset-top)',
+			marginRight: 'var(--safe-area-inset-right)',
+			marginBottom: 'var(--safe-area-inset-bottom)',
+			marginLeft: 'var(--safe-area-inset-left)',
 		},
 		'.mx-safe': {
-			marginRight: 'env(safe-area-inset-right)',
-			marginLeft: 'env(safe-area-inset-left)',
+			marginRight: 'var(--safe-area-inset-right)',
+			marginLeft: 'var(--safe-area-inset-left)',
 		},
 		'.my-safe': {
-			marginTop: 'env(safe-area-inset-top)',
-			marginBottom: 'env(safe-area-inset-bottom)',
+			marginTop: 'var(--safe-area-inset-top)',
+			marginBottom: 'var(--safe-area-inset-bottom)',
 		},
 		'.mt-safe': {
-			marginTop: 'env(safe-area-inset-top)',
+			marginTop: 'var(--safe-area-inset-top)',
 		},
 		'.mr-safe': {
-			marginRight: 'env(safe-area-inset-right)',
+			marginRight: 'var(--safe-area-inset-right)',
 		},
 		'.mb-safe': {
-			marginBottom: 'env(safe-area-inset-bottom)',
+			marginBottom: 'var(--safe-area-inset-bottom)',
 		},
 		'.ml-safe': {
-			marginLeft: 'env(safe-area-inset-left)',
+			marginLeft: 'var(--safe-area-inset-left)',
 		},
 		'.p-safe': {
-			paddingTop: 'env(safe-area-inset-top)',
-			paddingRight: 'env(safe-area-inset-right)',
-			paddingBottom: 'env(safe-area-inset-bottom)',
-			paddingLeft: 'env(safe-area-inset-left)',
+			paddingTop: 'var(--safe-area-inset-top)',
+			paddingRight: 'var(--safe-area-inset-right)',
+			paddingBottom: 'var(--safe-area-inset-bottom)',
+			paddingLeft: 'var(--safe-area-inset-left)',
 		},
 		'.px-safe': {
-			paddingRight: 'env(safe-area-inset-right)',
-			paddingLeft: 'env(safe-area-inset-left)',
+			paddingRight: 'var(--safe-area-inset-right)',
+			paddingLeft: 'var(--safe-area-inset-left)',
 		},
 		'.py-safe': {
-			paddingTop: 'env(safe-area-inset-top)',
-			paddingBottom: 'env(safe-area-inset-bottom)',
+			paddingTop: 'var(--safe-area-inset-top)',
+			paddingBottom: 'var(--safe-area-inset-bottom)',
 		},
 		'.pt-safe': {
-			paddingTop: 'env(safe-area-inset-top)',
+			paddingTop: 'var(--safe-area-inset-top)',
 		},
 		'.pr-safe': {
-			paddingRight: 'env(safe-area-inset-right)',
+			paddingRight: 'var(--safe-area-inset-right)',
 		},
 		'.pb-safe': {
-			paddingBottom: 'env(safe-area-inset-bottom)',
+			paddingBottom: 'var(--safe-area-inset-bottom)',
 		},
 		'.pl-safe': {
-			paddingLeft: 'env(safe-area-inset-left)',
+			paddingLeft: 'var(--safe-area-inset-left)',
 		},
 		'.min-h-screen-safe': {
 			minHeight: [
-				'calc(100vh - (env(safe-area-inset-top) + env(safe-area-inset-bottom)))',
+				'calc(100vh - (var(--safe-area-inset-top) + var(--safe-area-inset-bottom)))',
 				'-webkit-fill-available',
 			],
 		},
 		'.h-screen-safe': {
 			height: [
-				'calc(100vh - (env(safe-area-inset-top) + env(safe-area-inset-bottom)))',
+				'calc(100vh - (var(--safe-area-inset-top) + var(--safe-area-inset-bottom)))',
 				'-webkit-fill-available',
 			],
 		},
